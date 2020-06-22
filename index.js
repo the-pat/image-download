145e69133bfbd0a00af4e0f37be8094097bac86d
Patrick Tone <patrick@madronesoftware.com>
2020-06-22 18:51:22 -0500

    updates security vulnerabilities

--- index.js
+++ index.js
@@ -1,17 +1,15 @@
-'use strict';
+import got from "got";
+import imageType from "image-type";
+import isUrl from "is-url-superb";
 
-const got = require('got');
-const imageType = require('image-type');
-const isUrl = require('is-url-superb');
+export default async (url) => {
+  if (!(url && isUrl(url))) throw new TypeError("A valid url is required");
 
-module.exports = async(url) => {
-    if (!(url && isUrl(url))) throw new TypeError('A valid url is required');
+  const response = await got(url, { encoding: null });
+  const buffer = Buffer.from(response.body, "binary");
+  const type = imageType(buffer);
 
-    const response = await got(url, { encoding: null });
-    const buffer = Buffer.from(response.body, 'binary');
-    const type = imageType(buffer);
+  if (!type) return null;
 
-    if (!type) return null;
-
-    return buffer;
+  return buffer;
 };
--- test.js
+++ test.js
@@ -1,44 +1,44 @@
-'use strict';
+import test from "ava";
+import imageDownload from "./index.js";
 
-import test from 'ava';
-import imageDownload from './';
-
-test('returns buffer', async t => {
-    const value = await imageDownload('https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png');
-    t.true(Buffer.isBuffer(value));
+test("returns buffer", async (t) => {
+  const value = await imageDownload(
+    "https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png"
+  );
+  t.true(Buffer.isBuffer(value));
 });
 
-test('returns null', async t => {
-    const buffer = await imageDownload('https://patrickt.one');
-    t.is(buffer, null);
+test("returns null", async (t) => {
+  const buffer = await imageDownload("https://patrickt.one");
+  t.is(buffer, null);
 });
 
-test('rejects undefined url', async t => {
-    try {
-        await imageDownload(undefined);
-        t.fail();
-    } catch (err) {
-        t.true(err instanceof TypeError);
-        t.is(err.message, 'A valid url is required');
-    }
+test("rejects undefined url", async (t) => {
+  try {
+    await imageDownload(undefined);
+    t.fail();
+  } catch (err) {
+    t.true(err instanceof TypeError);
+    t.is(err.message, "A valid url is required");
+  }
 });
 
-test('rejects null url', async t => {
-    try {
-        await imageDownload(null);
-        t.fail();
-    } catch (err) {
-        t.true(err instanceof TypeError);
-        t.is(err.message, 'A valid url is required');
-    }
+test("rejects null url", async (t) => {
+  try {
+    await imageDownload(null);
+    t.fail();
+  } catch (err) {
+    t.true(err instanceof TypeError);
+    t.is(err.message, "A valid url is required");
+  }
 });
 
-test('rejects empty string url', async t => {
-    try {
-        await imageDownload('');
-        t.fail();
-    } catch (err) {
-        t.true(err instanceof TypeError);
-        t.is(err.message, 'A valid url is required');
-    }
+test("rejects empty string url", async (t) => {
+  try {
+    await imageDownload("");
+    t.fail();
+  } catch (err) {
+    t.true(err instanceof TypeError);
+    t.is(err.message, "A valid url is required");
+  }
 });
